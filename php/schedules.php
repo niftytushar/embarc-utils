a3bf384209ec40dcf08dfa14c6fd7f9b454a15b5
Tushar Agarwal <niftytushar@gmail.com>
2014-01-27 16:28:29 +0530

    bug fix for server unable to connect

--- php/schedules.php
+++ php/schedules.php
@@ -1,7 +1,7 @@
 <?php
 require_once "mysql_interface.php";
 require_once "servers.php";
-require "PHPMailer/PHPMailerAutoload.php";
+require_once "smtp.php";
 
 $schedules = new SCHEDULES();
 $schedules->checkAllServers();
@@ -10,32 +10,57 @@ class SCHEDULES
 {
 	public $mInterface;
 	public $servers;
+	public $smtp_obj;
 
 	public function __construct() {
 		$this->mInterface = new MYSQL_INTERFACE();
 		$this->servers = new SERVER();
+		$this->smtp_obj = new SMTPs();
 	}
 	
 	public function checkAllServers() {
 		$list_of_servers = $this->servers->getServersList();
 		$mailBody = "";
-		$mailAltBody = "";
 		
 		for($i = 0; $i < count($list_of_servers); $i++) {
 			$server_status = $this->servers->getStatus($list_of_servers[$i]["ip_address"]);
 			$server_status = json_decode($server_status);
-			if($server_status->updating->status == -1) {
-				$status = array("updating"=>$server_status->updating, "info"=>$list_of_servers[$i]);
-				$mailBody .= $this->getMailBody($status);
-				$mailAltBody .= $this->getMailAltBody($status);
+			
+			if(gettype($server_status) != "object") { // unable to connect to server
+				$status = array("updating"=>-1, "info"=>$list_of_servers[$i]);
+				$mailBody .= $this->getBody_UC($status);
+			} else {
+				if($server_status->updating->status == -1) { // AVL data not received
+					$status = array("updating"=>$server_status->updating, "info"=>$list_of_servers[$i]);
+					$mailBody .= $this->getBody_AVL($status);
+				}
 			}
 		}
 		
 		// finally send a mail
-		$this->mail_serverDown($mailBody, $mailAltBody);
+		$result = $this->smtp_obj->sendMail(null, [array("name"=>"Pradeep Jain", "email"=>"pradeep.brisk@gmail.com")], "Server Down", $mailBody, 0);
+		
+		if($result == "SUCCESS") echo "Mail sent\n";
 	}
 	
-	public function getMailBody($status) {
+	public function getBody_UC($status) {
+		return '<html>
+		<head></head>
+		<body>
+		Unable to connect to server <b>' . $status["info"]["company"] . '</b> <br />
+		<table>
+		<tr><td>Contact Name</td><td>'.$status["info"]["contact"].'</td></tr>
+		<tr><td>Email ID</td><td>'.$status["info"]["email"].'</td></tr>
+		<tr><td>IP</td><td>'.$status["info"]["ip_address"].'</td></tr>
+		<tr><td>URL</td><td><a href="http://' . $status["info"]["url"] . '">'.$status["info"]["url"].'</a></td></tr>
+		<tr><td>Software Version</td><td>'.$status["info"]["sw_version"].'</td></tr>
+		</table>
+		</body>
+		</html><br />';
+	}
+	
+	// get mail body for servers with errors
+	public function getBody_AVL($status) {
 		return '<html>
 		<head></head>
 		<body>
@@ -50,82 +75,5 @@ class SCHEDULES
 		</body>
 		</html><br />';
 	}
-	
-	public function getMailAltBody($status) {
-		return 'AVL data was <b>NOT</b> received on the server ' . $status["info"]["company"] . ' in last ' . intval($status["updating"]->interval) / 60 . ' minutes';
-	}
-	
-	public function mail_serverDown($mailBody, $mailAltBody) {
-		$mail = new PHPMailer;
-
-		$mail->isSMTP();										// Set mailer to use SMTP
-		$mail->SMTPDebug = 0;									// Production mode
-		$mail->Host = 'smtp.mandrillapp.com';							// Specify main and backup server
-		$mail->Port = 587;										// SMTP port for Gmail
-		$mail->SMTPAuth = true;									// Enable SMTP authentication
-		$mail->SMTPKeepAlive = true;							// SMTP connection will not close after each email sent, reduces SMTP overhead
-		$mail->SMTPSecure = 'tls';
-		$mail->Username = 'pradeep.brisk@gmail.com';		// SMTP username
-		$mail->Password = '6JKHeUjlpPsrCjxaNbfUsA';							// SMTP password
-		
-		$mail->From = 'support@findnsecure.com';				// Sender Email ID
-		$mail->FromName = 'Server Status';						// Sender Name
-		$mail->addAddress('anshul.agarwal@findnsecure.com', 'Anshul Agarwal');	// Recipient name - 1
-		//$mail->addAddress('tushar.agarwal@softcruise.com', 'Tushar Agarwal');  // Recipient name - 2
-		$mail->addReplyTo('support@findnsecure.com', 'Servers');
-		
-		$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-		$mail->isHTML(true);                                  // Set email format to HTML
-
-		$mail->Subject = "Server Down";
-		$mail->Body    = $mailBody;
-		$mail->AltBody = $mailAltBody;
-
-		if(!$mail->send()) {
-		   echo 'Message could not be sent.';
-		   echo 'Mailer Error: ' . $mail->ErrorInfo;
-		   exit;
-		}
-
-		echo "Mail sent\n";
-	}
-	/*
-	public function mail_serverDown($status) {
-		$from = "Server Status <support@findnsecure.com>";
-
-		$to=" <pradeep.jain@findnsecure.com>, Tushar Agarwal <tushar.agarwal@softcruise.com>";
-
-		// To send HTML mail, the Content-type header must be set
-		$headers  = 'MIME-Version: 1.0' . "\r\n";
-		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-		// Additional headers
-		$headers .= "From: $from\r\n";
-
-		//mail subject
-		$subject = $status["info"]["company"] . ": Server Down";
-
-		//mail body
-		$message = '<html>
-		<head></head>
-		<body>
-		AVL data was not received on the server ' . $status["info"]["company"] . ' in last ' . intval($status["updating"]->interval) / 60 . ' minutes. <br />
-		<table>
-		<tr><td>Contact Name</td><td>'.$status["info"]["contact"].'</td></tr>
-		<tr><td>Email ID</td><td>'.$status["info"]["email"].'</td></tr>
-		<tr><td>IP</td><td>'.$status["info"]["ip_address"].'</td></tr>
-		<tr><td>URL</td><td><a href="http://' . $status["info"]["url"] . '">'.$status["info"]["url"].'</a></td></tr>
-		<tr><td>Software Version</td><td>'.$status["info"]["sw_version"].'</td></tr>
-		</table>
-		</body>
-		</html>';
-
-		//send email
-		mail($to,$subject,$message,$headers);
-		
-		// log mail sent
-		echo "Mail sent to $to for server " . $status["info"]["company"];
-	}
-	*/
 }
 ?>

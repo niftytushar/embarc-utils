923176de77e58b2e7638f53e6e7a7348e82e35e9
Tushar Agarwal <niftytushar@gmail.com>
2014-01-27 16:29:00 +0530

    using common mail function now

--- php/smtp.php
+++ php/smtp.php
@@ -13,52 +13,62 @@ class SMTPs
 		$this->users = new USERS();
 	}
 	
-	// save a new user
-	public function sendMail($postData) {
+	// test a specified SMTP server
+	public function testSMTPDetails($postData) {
 		$username = $_SESSION['user'];
 		
 		// details of user, to send email to
 		$userDetails = $this->users->getUser($username);
-		$userDetails = $userDetails[0];
 		
 		// data received from client
-		$host = $postData['host'];
-		$port = $postData['port'];
-		$username = $postData['username'];
-		$password = $postData['password'];
-		$enAuth = $postData['enAuth'] == 1?true:false;
-		$enSSL = $postData['enSSL'] == 1?true:false;
+		$settings = array("host"=>$postData['host'], "port"=>$postData['port'], "username"=>$postData['username'], "password"=>$postData['password'], "enAuth"=>($postData['enAuth'] == 1?true:false), "enSSL"=>($postData['enSSL'] == 1?true:false));
+		
+		$result = $this->doMail($settings, $userDetails, "Congrats! SMTP server works", "This is a test mail from ".$settings['host'].". If you've received this mail, the SMTP server settings are correct.", 2);
+		
+		if($result == "SUCCESS") return "Mail sent successfully.";
+	}
+	
+	public function sendMail($settings, $recipients, $subject="embarc-utils", $body, $debugMode=0) {
+		if(!$settings) $settings = $this->_defaultSMTPSettings();
 		
 		// start PHP mailer setup
 		$mail = new PHPMailer;
 		$mail->isSMTP();															// Set mailer to use SMTP
-		$mail->SMTPDebug = 2;														// Debug mode - Commands + Data
+		$mail->SMTPDebug = $debugMode;												// Debug mode - Commands + Data
 		$mail->Debugoutput = "html";												// HTML style debug output
-		$mail->Host = $host;														// SMTP server
-		$mail->Port = $port;														// SMTP port [Gmail - (ssl: 465/25, tls: 587)]
-		$mail->SMTPAuth = $enAuth;													// Enable SMTP authentication
-		if($enSSL) $mail->SMTPSecure = 'ssl';										// Enable encryption
+		$mail->Host = $settings['host'];											// SMTP server
+		$mail->Port = $settings['port'];											// SMTP port [Gmail - (ssl: 465/25, tls: 587)]
+		$mail->SMTPAuth = $settings['enAuth'];										// Enable SMTP authentication
+		if($settings['enSSL']) $mail->SMTPSecure = 'ssl';							// Enable encryption
 		$mail->SMTPKeepAlive = false;												// SMTP connection will close after sending the email
-		$mail->Username = $username;												// SMTP username
-		$mail->Password = $password;												// SMTP password
+		$mail->Username = $settings['username'];									// SMTP username
+		$mail->Password = $settings['password'];									// SMTP password
 		$mail->From = 'check@embarc.com';											// Email ID of sender
-		$mail->FromName = 'Check';													// Name of sender
-		$mail->addAddress($userDetails['email'], $userDetails['name']);				// Details of receipent
+		$mail->FromName = 'embarc-utils';											// Name of sender
+		
+		// Adding recipients		
+		for($i=0; $i<count($recipients); $i++) {
+			$mail->addAddress($recipients[$i]['email'], $recipients[$i]['name']);	// Details of receipent
+		}
+		
 		$mail->addReplyTo('noreply@embarc.com', 'No-reply');						// Add reply-to address
 		$mail->WordWrap = 50;														// Set word wrap to 50 characters
 		$mail->isHTML(true);														// Set email format to HTML
 		
 		// the mail
-		$mail->Subject = "Congrats! SMTP server works";
-		$mail->Body    = "This is a test mail from $host. If you've received this mail, the SMTP server settings are correct.";
-		$mail->AltBody = "This is a test mail from $host. If you receive this mail, the SMTP server settings are correct.";
+		$mail->Subject = $subject;
+		$mail->Body = $body;
 		
 		// sending email
 		if(!$mail->send()) {
 		   return $mail->ErrorInfo;
 		}
 		
-		return "Mail sent successfully.";
+		return "SUCCESS";
+	}
+	
+	public function _defaultSMTPSettings() {
+		return array("host"=>"smtp.mandrillapp.com", "port"=>"587", "username"=>"pradeep.brisk@gmail.com", "password"=>"6JKHeUjlpPsrCjxaNbfUsA", "enAuth"=>true, "enSSL"=>false);
 	}
 }
 ?>

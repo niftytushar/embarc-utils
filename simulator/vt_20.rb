48fbf4c41526ec7cbba55f4d2e70b8aed61e3b5e
Tushar Agarwal <niftytushar@gmail.com>
2014-02-24 12:35:16 +0530

    updated simulator data

--- simulator/vt_20.rb
+++ simulator/vt_20.rb
@@ -2,44 +2,47 @@ require 'socket'
 #require 'csv'
 require 'nokogiri'
 
-hostname = '117.241.152.36'
+hostname = 'mycanvasback.com'
 port = 21000
 
 count = 1
 
 # parse GPX file using Nokogiri - meaning handsaw
-gpx_file = Nokogiri::XML(File.open('data_1.gpx'))
+gpx_file = Nokogiri::XML(File.open('data.gpx'))
 trkpts = gpx_file.css('xmlns|trkpt')
+while true do
+	speed = 0
+	trkpts.each do |trkpt|		
+		# current time in UTC
+		time = Time.now.utc.strftime "%Y%m%d%H%M%S"
+		
+		# location from file
+		latitude = trkpt.xpath('@lat').to_s.to_f
+		longitude = trkpt.xpath('@lon').to_s.to_f
 
-trkpts.each do |trkpt|	
-	# current time in UTC
-	time = Time.now.utc.strftime "%Y%m%d%H%M%S"
-	
-	# location from file
-	latitude = trkpt.xpath('@lat').to_s.to_f
-	longitude = trkpt.xpath('@lon').to_s.to_f
-
-	# data packet
-	some_data = "2000000011,#{time},#{longitude},#{latitude},71,25,0,8,0,7,2,0.50,0.60,0,0"
-	
-	# open a socket connection to the server
-	sock = TCPSocket.open(hostname, port)
-	
-	# send some data
-	sock.print(some_data)
-	
-	# close the connection
-	sock.close
-	
-	# debug
-	puts "#{count} sent at #{time}"
-	
-	count += 1
-	
-	# wait time
-	sleep 1
+		# data packet
+		some_data = "2000000011,#{time},#{longitude},#{latitude},#{speed},25,0,8,0,7,2,0.50,0.60,0,0"
+		
+		# open a socket connection to the server
+		sock = TCPSocket.open(hostname, port)
+		
+		# send some data
+		sock.print(some_data)
+		
+		# close the connection
+		sock.close
+		
+		# debug
+		puts "#{count} sent at #{time}"
+		
+		count += 1
+		
+		speed += 1
+		
+		# wait time
+		sleep 1
+	end
 end
-
 =begin
 # reading a CSV file
 CSV.foreach("data_2.csv") do |row|	
